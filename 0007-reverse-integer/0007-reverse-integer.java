ac05c83baea276021cf7330badb1baa65136674a
Soumya Raj <soumya2115@gmail.com>
2026-01-02 23:52:07 +0530

    Time: 1 ms (99.76%), Space: 42.5 MB (63.08%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int reverse(int n) {
+        int rev = 0;
+
+        while (n != 0) {
+            int r = n % 10;
+            if (rev > Integer.MAX_VALUE / 10 || rev < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+
+            rev = rev * 10 + r;
+            n = n / 10;
+        }
+        return rev;
+    }
+}
