484d5d3d58c13873135b67780a0ec2465300e4d3
Soumya Raj <soumya2115@gmail.com>
2026-05-20 00:01:11 +0530

    Time: 1 ms (94.33%), Space: 42.8 MB (13.12%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -1,28 +1,39 @@
 class Solution {
     public int divide(int dividend, int divisor) {
-        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+
+        boolean negative = (dividend < 0) ^ (divisor < 0);
+
+        long dvd = Math.abs((long) dividend);
+        long dvs = Math.abs((long) divisor);
+
+        long result = 0;
+
+        while (dvd >= dvs) {
+
+            long temp = dvs;
+            long multiple = 1;
+
+            while (dvd >= (temp << 1)) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+
+            dvd -= temp;
+            result += multiple;
+        }
+
+        if (negative) {
+            result = -result;
+        }
+
+        if (result > Integer.MAX_VALUE) {
             return Integer.MAX_VALUE;
         }
-        int sign=1;
-        if((dividend<0 && divisor>0) || (dividend>0 && divisor<0)){
-            sign=-1;
+
+        if (result < Integer.MIN_VALUE) {
+            return Integer.MIN_VALUE;
         }
-        long dvd=Math.abs((long)dividend);
-        long dvs=Math.abs((long)divisor);
-        int quotient=0;
-        while(dvd>=dvs){
-            long temp=dvs;
-            int multiple=1;
-            while(dvd>=(temp<<1)){
-                temp=temp<<1;
-                multiple=multiple<<1;
-            }
-            dvd=dvd-temp;
-            quotient=quotient+multiple;
-        }
-        if(sign==-1){
-            quotient=-quotient;
-        }
-        return quotient;
+
+        return (int) result;
     }
 }
