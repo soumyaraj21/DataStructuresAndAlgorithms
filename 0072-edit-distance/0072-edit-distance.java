2a1560488ad35bd15cee94581bc8f002aeba38ba
Soumya Raj <soumya2115@gmail.com>
2026-06-29 17:53:32 +0530

    Time: 4 ms (77.66%), Space: 46.4 MB (94.93%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        int[] prev = new int[m + 1];
+        for (int j = 0; j <= m; j++) {
+            prev[j] = j;
+        }
+        for (int i = 1; i <= n; i++) {
+            int[] curr = new int[m + 1];
+            curr[0] = i;
+            for (int j = 1; j <= m; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    curr[j] = prev[j - 1];
+                } else {
+                    int insert = curr[j - 1];
+                    int delete = prev[j];
+                    int replace = prev[j - 1];
+                    curr[j] = 1 + Math.min(insert,Math.min(delete, replace));
+                }
+            }
+            prev = curr;
+        }
+        return prev[m];
+    }
+}
